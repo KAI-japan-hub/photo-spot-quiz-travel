47751957e689cf10d6c2b23ef1fc982e1bf3b774
山口翔生 <yamaguchishousei@kids108.kyoto-su.ac.jp>
2025-09-26 18:38:24 +0900

    Add Kyovel app (code only)

---
+++ KyovelTests/KaiKaiTests.swift
@@ -0,0 +1,17 @@
+//
+//  KaiKaiTests.swift
+//  KaiKaiTests
+//
+//  Created by 山口翔生 on 2025/04/30.
+//
+
+import Testing
+@testable import KaiKai
+
+struct KaiKaiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
